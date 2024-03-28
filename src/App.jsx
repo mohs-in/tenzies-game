c1c7f3fa0975ca5f70c6817f4e770556cea00b25
mohs-in <in.mohsin@outlook.com>
2024-03-28 12:15:44 +0530

    roll btn added to generate random values

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,26 @@ import './App.css'
 
 function App() {
 
-  let numArr = []
-  for(let i=0;i<10;i++) {
-    numArr.push(Math.floor(Math.random()*6+1))
+  const [die, setDie] = useState(getRandomArr)
+
+  function getRandomArr() {
+    let dieArray = []
+    for(let i=0;i<10;i++) {
+      dieArray.push(Math.ceil(Math.random()*6))
+    }
+    return dieArray
   }
-    
+
+  function rollDie() {
+    setDie(getRandomArr())
+  }
+
   return (
     <main>
       <div className='die-container'>
-        {numArr.map(num => <Die value={num} />)}
+        {die.map(num => <Die value={num} />)}
       </div>
+      <button onClick={rollDie} className='btn-roll'>Roll</button>
     </main>
   )
 }
