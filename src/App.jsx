991cb3b3d8a3d4f006c74bb77b71583225a5d338
mohs-in <in.mohsin@outlook.com>
2024-03-28 15:22:45 +0530

    add nanoid package

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { React, useState } from 'react'
 import Die from '../components/Die.jsx'
+import { nanoid } from 'nanoid'
 import './App.css'
 
 function App() {
@@ -11,9 +12,10 @@ function App() {
     for(let i=0;i<10;i++) {
       dieArray.push(
         { 
+          id:nanoid(),
           value: Math.ceil(Math.random()*6), 
           isHeld: false 
-        }
+        }        
       )
     }
     return dieArray
@@ -26,7 +28,7 @@ function App() {
   return (
     <main>
       <div className='die-container'>
-        {die.map(obj => <Die value={obj.value} />)}
+        {die.map(obj => <Die key={obj.id} value={obj.value} />)}
       </div>
       <button onClick={rollDie} className='btn-roll'>Roll</button>
     </main>
